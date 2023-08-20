5769be05d8d3b9de228eb06478c9f861ebdccc86
Nadun Vindiya <nadunbalasuriya03@gmail.com>
2023-08-21 01:25:20 +0800

    Test

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,26 @@ import { ref } from 'vue';
 
 const title = ref("TODO-APP")
 const center = ref("center")
+const task = ref('')
+var id = ref(0)
+var newtask = ref('')
+
+const todos = ref([])
 </script>
 
 <template>
   <div :class="center">
     <h1>{{ title }} </h1>
-    <input type="text" ref="todo">
+    <p>{{ task }}</p>
+    <input v-model="task" type="text" ref="todo">
     <button>Add</button>
   </div>
 </template>
 
 
 <style scoped>
-.center{
+.center {
   display: flex;
   flex-direction: column;
 }
-
 </style>
