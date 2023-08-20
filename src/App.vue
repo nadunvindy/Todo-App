a38db523a73101fbb7cb3ebc2717accf1cc3d728
Nadun Vindiya <nadunbalasuriya03@gmail.com>
2023-08-21 01:08:00 +0800

    Base App

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+
+const title = ref("TODO-APP")
+const center = ref("center")
+</script>
+
+<template>
+  <div :class="center">
+    <h1>{{ title }} </h1>
+    <input type="text" ref="todo">
+    <button>Add</button>
+  </div>
+</template>
+
+
+<style scoped>
+.center{
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
